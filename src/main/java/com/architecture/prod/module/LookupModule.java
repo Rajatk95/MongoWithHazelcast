65e1ee3b3e891a5fb44adf9a89b4491df676b60c
Rajat khandelwal <Rajatk95@users.noreply.github.com>
2018-10-21 09:55:34 +0530

    formatted code

--- src/main/java/com/architecture/prod/module/LookupModule.java
+++ src/main/java/com/architecture/prod/module/LookupModule.java
@@ -13,8 +13,8 @@ public class LookupModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		install(new ServletModule());
-    install(new LookupCacheModule());
-    bind(LookupService.class).to(LookupServiceImpl.class);
+		install(new LookupCacheModule());
+		bind(LookupService.class).to(LookupServiceImpl.class);
 		bind(LookupRepository.class);
 		bind(Datastore.class).toProvider(DataStoreProvider.class);
 	}
